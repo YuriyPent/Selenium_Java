364107eb70389d4d360acbba27359fadf0b7765b
Yuriy <Seaman_on_watch@mail.ru>
2018-11-25 22:15:13 +0300

    Использую Selenide в тестах

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -1,91 +1,94 @@
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static java.lang.String.format;
-import static org.openqa.selenium.By.*;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+import static org.openqa.selenium.By.xpath;
 //https://www.spotify.com/us/signup/
 
 public class SignUpPage {
-    private WebDriver driver;
-
-    public SignUpPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-   private By emailField = By.cssSelector("input#register-email");
-   private By confirmEmailField = By.cssSelector("input#register-confirm-email");
-   private By passwordField = By.cssSelector("input#register-password");
-   private By nameField = By.cssSelector("input#register-displayname");
-   private By monthDropDown = By.cssSelector("select#register-dob-month");
-   private String monthDropDownOption = "//select[@id='register-dob-month']/option[text()='%s']";
-   private By dayField = By.cssSelector("input#register-dob-day");
-   private By yearField = By.cssSelector("input#register-dob-year");
-   private String sexRadioButton = "//li[@id=\"li-gender\"]/label[normalize-space()='%s']/input";
-   private By shareCheckbox = By.cssSelector("input#register-thirdparty");
-   private By registerButton = By.cssSelector("a#register-button-email-submit");
-   private By errorLabel = xpath("//label[@class='has-error' and string-length(text())>0]");
-   private String errorByText = "//label[@class=\"has-error\" and text()=\"%s\"]";
-
-    public SignUpPage typeEmail (String email){
-        driver.findElement(emailField).sendKeys(email);
-        return this;
-    }
-    public SignUpPage typeConfirmEmail (String email){
-        driver.findElement(confirmEmailField).sendKeys(email);
-        return this;
-    }
-    public SignUpPage typePassword (String password){
-        driver.findElement(passwordField).sendKeys(password);
-        return this;
-    }
-    public SignUpPage typeName (String name){
-        driver.findElement(nameField).sendKeys(name);
-        return this;
-    }
-    public SignUpPage setMonth (String month){
-        driver.findElement(monthDropDown).click();
-        new WebDriverWait(driver, 5).
-                until(visibilityOfElementLocated(xpath(format(monthDropDownOption, month)))).click();
-        return this;
-    }
-    public SignUpPage typeDay (String day){
-        driver.findElement(dayField).sendKeys(day);
-        return this;
-    }
-    public SignUpPage typeYear (String year){
-        driver.findElement(yearField).sendKeys(year);
-        return this;
-    }
-    public SignUpPage setSex (String value){
-        driver.findElement(xpath(format(sexRadioButton, value))).click();
-        return this;
-    }
-    public SignUpPage setShare (boolean value){
-        WebElement checkbox = driver.findElement(shareCheckbox);
-        if (!checkbox.isSelected() == value){
-            checkbox.click();
-        }
-        return this;
-    }
-    public void clickSignUpButton (){
-        driver.findElement(registerButton).click();
-    }
-    public List<WebElement> getErrors(){
-        return  driver.findElements(errorLabel);
-    }
-    public String getErrorByNumber(int number){
-        return  getErrors().get(number - 1).getText();
-    }
-    public boolean isErrorVisible(String message){
-        return driver.findElements(xpath(format(errorByText, message))).size() > 0
-                && driver.findElements(xpath(format(errorByText, message))).get(0).isDisplayed();
-    }
 
 
+    private By emailField = By.cssSelector("input#register-email");
+    private By confirmEmailField = By.cssSelector("input#register-confirm-email");
+    private By passwordField = By.cssSelector("input#register-password");
+    private By nameField = By.cssSelector("input#register-displayname");
+    private By monthDropDown = By.cssSelector("select#register-dob-month");
+    private String monthDropDownOption = "//select[@id='register-dob-month']/option[text()='%s']";
+    private By dayField = By.cssSelector("input#register-dob-day");
+    private By yearField = By.cssSelector("input#register-dob-year");
+    private String sexRadioButton = "//li[@id='li-gender']//label[normalize-space()='Male']/input";
+    private By shareCheckbox = By.cssSelector("input#register-thirdparty");
+    private By registerButton = By.cssSelector("a#register-button-email-submit");
+    private By errorLabel = xpath("//label[@class='has-error' and string-length(text())>0]");
+    private String errorByText = "//label[@class=\"has-error\" and text()=\"%s\"]";
 
+    public SignUpPage open() {
+        Selenide.open("/");
+        return this;
+    }
+
+    public SignUpPage typeEmail(String email) {
+        $(emailField).setValue(email);
+        return this;
+    }
+
+    public SignUpPage typeConfirmEmail(String email) {
+        $(confirmEmailField).setValue(email);
+        return this;
+    }
+
+    public SignUpPage typePassword(String password) {
+        $(passwordField).setValue(password);
+        return this;
+    }
+
+    public SignUpPage typeName(String name) {
+        $(nameField).setValue(name);
+        return this;
+    }
+
+    public SignUpPage setMonth(String month) {
+        $(monthDropDown).selectOption(month);
+        return this;
+    }
+
+    public SignUpPage typeDay(String day) {
+        $(dayField).setValue(day);
+        return this;
+    }
+
+    public SignUpPage typeYear(String year) {
+        $(yearField).setValue(year);
+        return this;
+    }
+
+    public SignUpPage setSex(String value) {
+        $(By.xpath(format(sexRadioButton, value))).click();
+        return this;
+    }
+
+    public SignUpPage setShare(boolean value) {
+        $(shareCheckbox).setSelected(value);
+        return this;
+    }
+
+    public void clickSignUpButton() {
+        $(registerButton).click();
+    }
+
+    public ElementsCollection getErrors() {
+        return $$(errorLabel);
+    }
+
+    public SelenideElement getErrorByNumber(int number) {
+        return getErrors().get(number - 1);
+    }
+
+    public SelenideElement getError(String message) {
+        return $(xpath(format(errorByText, message)));
+    }
 }
--- src/test/java/SignUpTests.java
+++ src/test/java/SignUpTests.java
@@ -1,77 +1,77 @@
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
 
 
 public class SignUpTests {
-     private WebDriver driver;
-     private SignUpPage page;
 
-     @Before
-     public void setUp(){
-          System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver.exe");
-          driver = new FirefoxDriver();
-          driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-          driver.get("https://www.spotify.com/us/signup/");
-     }
+    private SignUpPage page;
 
-     @Test
-     public void typeInvalidYear(){
-          page = new SignUpPage(driver);
-          page.setMonth("December").typeDay("12").typeYear("77").setShare(true);
-          assertTrue(page.isErrorVisible("Please enter a valid year."));
-          assertFalse(page.isErrorVisible("When were you born?"));
-     }
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver.exe");
+        baseUrl = "https://www.spotify.com/us/signup";
+        //browser = "marionette";
+        //WebDriverRunner.setWebDriver(new FirefoxDriver());
+    }
 
-     @Test
-     public void typeInvalidEmail(){
-          page = new SignUpPage(driver);
-          page.typeName("test@mail.test")
-                  .typeConfirmEmail("wrong@mail.test")
-                  .typeName("Testname")
-                  .clickSignUpButton();
-          assertTrue(page.isErrorVisible("Email address doesn't match."));
-     }
+    @Test
+    public void typeInvalidYear() {
+        page = new SignUpPage();
+        page.open()
+                .setMonth("December")
+                .typeDay("12")
+                .typeYear("77")
+                .setShare(true);
+        page.getError("Please enter a valid year.")
+                .shouldBe(visible);
+        page.getError("When were you born?")
+                .shouldNotBe(visible);
+    }
 
-     @Test
-     public void signupWithEmptyPassword(){
-          page = new SignUpPage(driver);
-          page.typeEmail("test@mail.test")
-                  .typeConfirmEmail("test@mail.test")
-                  .typeName("Testname")
-                  .clickSignUpButton();
-          assertTrue(page.isErrorVisible("Please choose a password."));
-     }
+    @Test
+    public void typeInvalidEmail() {
+        page = new SignUpPage();
+        page.open()
+                .typeName("test@mail.test")
+                .typeConfirmEmail("wrong@mail.test")
+                .typeName("Testname")
+                .clickSignUpButton();
+        page.getError("Email address doesn't match.")
+                .shouldBe(visible);
+    }
 
-     @Test
-     public void typeinvalidValues(){
-          page = new SignUpPage(driver);
-          page.typeEmail("testmail")
-                  .typeConfirmEmail("wrong@test.mail")
-                  .typePassword("qwerty1234")
-                  .typeName("Name")
-                  .setSex("Male")
-                  .setShare(false)
-                  .clickSignUpButton();
-          Assert.assertEquals(6, page.getErrors().size());
-          Assert.assertEquals("Please enter your birth month.",
-                  page.getErrorByNumber(3));
+    @Test
+    public void signupWithEmptyPassword() {
+        page = new SignUpPage();
+        page.open()
+                .typeEmail("test@mail.test")
+                .typeConfirmEmail("test@mail.test")
+                .typeName("Testname")
+                .clickSignUpButton();
+        page.getError("Please choose a password.")
+                .shouldBe(visible);
+    }
 
-     }
+    @Test
+    public void typeinvalidValues() {
+        page = new SignUpPage();
+        page.open()
+                .typeEmail("testmail")
+                .typeConfirmEmail("wrong@test.mail")
+                .typePassword("qwerty1234")
+                .typeName("Name")
+                .setSex("Male")
+                .setShare(false)
+                .clickSignUpButton();
+        page.getErrors().shouldHave(size(6));
+        page.getErrorByNumber(3)
+                .shouldHave(text("Please enter your birth month."));
 
-     @After
-     public void tearDown(){
-          driver.quit();
-     }
-
-
+    }
 
 }
